172676133858d95bcbb3235ab58abdf001270246
Mario Greco <mrgreco3@gmail.com>
2012-03-07 16:51:55 +0100

    ControlloCartaController: inserito controller per visualizzare stato carta

---
+++ Classes/ControlloCartaController.h
@@ -0,0 +1,13 @@
+//
+//  ControlloCartaController.h
+//  PerDueCItyCard
+//
+//  Created by mario greco on 07/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface ControlloCartaController : UIViewController
+
+@end
