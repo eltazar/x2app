87f7e4b3a60e4f80654c256b2569247b97dec8e4
Mario Greco <mrgreco3@gmail.com>
2012-03-07 15:11:43 +0100

    RichiediCartaViewController: creato controller per richiesta carta

---
+++ Classes/RichiediCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  RichiediCardViewController.h
+//  PerDueCItyCard
+//
+//  Created by mario greco on 07/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RichiediCardViewController : UITableViewController
+
+@end
