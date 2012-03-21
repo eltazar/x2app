76f56ab4c6db31a01257c16184847dfe52af042c
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2012-03-21 17:13:00 +0100

    LocalDatabaseAccess: aggiunta classe per accesso a storace locale

---
+++ Classes/LocalDatabaseAccess.h
@@ -0,0 +1,20 @@
+//
+//  LocalDatabaseAccess.h
+//  PerDueCItyCard
+//
+//  Created by Gabriele "Whisky" Visconti on 21/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "CartaPerDue.h"
+
+@interface LocalDatabaseAccess : NSObject
+
+// Valutare se alcuni devono essere metodi di istanza invece che di classe
++ (NSArray *) fetchStoredCardsAndWriteErrorIn:(NSError **)error;
+//+ (void) storeCard:(CartaPerDue *) card;
+//+ (void) removeStoredCard:(CartaPerDue *) card;
+
+@end
---
+++ Classes/LocalDatabaseAccess.m
@@ -0,0 +1,61 @@
+//
+//  LocalDatabaseAccess.m
+//  PerDueCItyCard
+//
+//  Created by Gabriele "Whisky" Visconti on 21/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "LocalDatabaseAccess.h"
+#import "PerDueCItyCardAppDelegate.h"
+
+@implementation LocalDatabaseAccess
+
+
++ (NSArray *) fetchStoredCardsAndWriteErrorIn:(NSError **)error{
+    //Otteniamo il puntatore al NSManagedContext
+    PerDueCItyCardAppDelegate *appDelegate = (PerDueCItyCardAppDelegate *)[[UIApplication sharedApplication] delegate];
+    
+    NSManagedObjectContext *context = [appDelegate managedObjectContext];
+    
+    //istanziamo la classe NSFetchRequest
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init]; 
+    
+    //istanziamo l'Entità da passare alla Fetch Request
+    NSEntityDescription *entity = [NSEntityDescription 
+                                   entityForName:@"CartaPerDue" inManagedObjectContext:context];
+    //Settiamo la proprietà Entity della Fetch Request
+    [fetchRequest setEntity:entity];
+    
+    //Eseguiamo la Fetch Request e salviamo il risultato in un array, per visualizzarlo nella tabella
+    NSArray *fetchedObjects = [context executeFetchRequest:fetchRequest error:error];
+    [fetchRequest release];
+    
+    
+    
+    //se > 0 ci sono righe nella tabella, ovvero carte registrate
+    if(fetchedObjects && fetchedObjects.count > 0){
+        
+        //        NSLog(@ " --------> FO = %@ \n, fo count = %d, \n titolare = %@, carta = %@, scadenza = %@",tempArray,tempArray.count, [[tempArray objectAtIndex:0] valueForKey:@"titolare"], [[tempArray objectAtIndex:0] valueForKey:@"numero"], [[tempArray objectAtIndex:1] valueForKey:@"scadenza"]);
+        NSLog(@"Did fetch this stuff: %@", fetchedObjects);
+        NSMutableArray *tempCards = [[NSMutableArray alloc] init];
+        for (int i = 0; i < fetchedObjects.count; i++) {
+            CartaPerDue *carta = [[CartaPerDue alloc] init];
+            NSManagedObject *fetchedObject = [fetchedObjects objectAtIndex:i];
+            NSLog(@"Parsing object: %@", fetchedObject);
+            carta.name = [fetchedObject valueForKey:@"nome"];
+            carta.surname = [fetchedObject valueForKey:@"cognome"];
+            carta.number = [fetchedObject valueForKey:@"numero"];
+            carta.expiryDate2 = [fetchedObject valueForKey:@"scadenza"];
+            NSLog(@"Scadenza è di tipo: %@ e contiene: %@", [carta.expiryDate2 class], carta.expiryDate2);
+            [tempCards addObject:carta];
+        }
+        NSArray *cardsArray = [[[NSArray alloc] initWithArray:tempCards] autorelease];
+        [tempCards release];
+        return  cardsArray;
+    }
+    return [[[NSArray alloc] init] autorelease];
+    
+}
+
+@end
