ef3760a48916c92ddaeb170d8ec57fec681470fb
Gabriele "Whisky" Visconti <fastfading@Mac-Pro-di-Gabriele.local>
2012-03-21 02:21:08 +0100

    Script PHP per il controllo dell'associazione delle carte ai device

---
+++ Classes/CardDeviceAssociation.php
@@ -0,0 +1,21 @@
+include 'QueryHelper.php'
+
+card_number = $_POST['card_number'];
+device_udid = $_POST['device_udid'];
+
+qh = new QueryHelper();
+
+query = 'SELECT udid_iphone FROM cartaperdue_app.abbinamento_carta_iphone WHERE codice_carta ='.$card_number;
+
+$json_result = $qh->query($query);
+$fetched_udid = json_decode($json_result);
+
+if ($device_udid==$fetched_udid){
+    $response = TRUE;
+} else {
+    $responde = FALSE;
+}
+
+echo json_encode($response);
+
+
