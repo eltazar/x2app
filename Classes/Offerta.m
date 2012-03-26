a54203c02b24f33d4774916d131e3c485f02fb6e
Mario Greco <mrgreco3@gmail.com>
2012-03-26 23:36:15 +0200

    Offerta: cambiato sfondo delle webview e colore dettaglio offerta

--- Classes/Offerta.m
+++ Classes/Offerta.m
@@ -291,7 +291,7 @@ didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 		titololabel.text=tit;
 //		NSString *sitesiText=[NSString stringWithFormat:@"<body bgcolor=\"#8E1507\"><font face=\"Helvetica regular\"><span style=\"color: #FFFFFF;\"><span style=\"font-size: 60%;\">%@</body>",[dict objectForKey:@"offerta_descrizione_breve"]];
         
-        NSString *sintesiText = [NSString stringWithFormat:@"<body bgcolor=\"#8E1507\"><div style=\"background-color:white; font-style:Helvetica regular;text-align:center;color:#4d4d4d;font-weight:bold;\"><p style=\"font-size:60px;\">%@</p></div> <div style=\"background-color:#8b1800; font-style:Helvetica regular;font-size:55px; color:white;\">%@</div></body>", [dict objectForKey:@"offerta_title"],[dict objectForKey:@"offerta_descrizione_breve"]];
+        NSString *sintesiText = [NSString stringWithFormat:@"<body bgcolor=\"#8E1507\"><div style=\"background-color:8b1800; font-style:Helvetica regular;text-align:center;color:white;font-weight:bold;\"><p style=\"font-size:60px;\">%@</p></div> <div style=\"background-color:#8b1800; font-style:Helvetica regular;font-size:55px; color:white;\">%@</div></body>", [dict objectForKey:@"offerta_title"],[dict objectForKey:@"offerta_descrizione_breve"]];
 		[insintesitextWebView loadHTMLString:sintesiText baseURL:nil];
         //		[comprasintesi setTitle: [NSString stringWithFormat:@"Compra subito! Solo %@€",[dict objectForKey:@"coupon_valore_acquisto"]] forState:UIControlStateNormal];
         //		[[comprasintesi layer] setCornerRadius:8.0f];
