e078f0e141c833554a22edd12fdba7ef3cd77305
Mario Greco <mrgreco3@gmail.com>
2012-04-03 17:48:35 +0200

    FindNearCompanyController: aggiunta classe per mostrare esercenti vicini e che accettano la card virtuale

---
+++ Classes/FindNearCompanyController.h
@@ -0,0 +1,14 @@
+//
+//  FindNearCompanyController.h
+//  PerDueCItyCard
+//
+//  Created by mario greco on 03/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DatabaseAccess.h"
+
+@interface FindNearCompanyController : UITableViewController<DatabaseAccessDelegate>
+
+@end
