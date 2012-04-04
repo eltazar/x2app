03fe4dcacc236aeca3f1f82090f739a7ee7b435a
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2012-04-04 19:41:23 +0200

    DettaglioEsercenteNew: aggiornati metodi delegato tabella alla presenza di idxMap.

--- Classes/DettaglioEsercenteNew.m
+++ Classes/DettaglioEsercenteNew.m
@@ -67,7 +67,7 @@
     [self.idxMap setKey:@"URL"              forSection:3 row:0];
     
     if (isCoupon || isGenerico) {
-        [self.idxMap removeKeyAtSection:0 row:2]; 
+        [self.idxMap removeKey:@"GiornoValidita"]; 
     }
     
     
@@ -147,7 +147,8 @@
 
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-	int righesecondasezione=3;
+    return [self.idxMap rowsInSection:section];
+	/*int righesecondasezione=3;
 	switch (section) {
 	case 0:
 		if ( [ [NSString stringWithFormat:@"%@",[self.dict objectForKey:@"Giorno_chiusura_Esercente"]] isEqualToString:@"<null>"] ){
@@ -174,17 +175,135 @@
 	default:
 		return 0;
 		break;
-	}
+	}*/
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
 	
 	static NSString *CellIdentifier = @"Cell";
+    NSString *key = [self.idxMap keyForIndexPath:indexPath];
     
+    
+#warning mettere il riuso al posto giusto
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
 	
-	if( (cell == nil) && (indexPath.section==0) && (indexPath.row==0) ) {
+	
+    if ([key isEqualToString:@"Indirizzo"]) {
+        [[NSBundle mainBundle] loadNibNamed:@"cellaindirizzo" owner:self options:NULL];
+        cell = cellaindirizzo;
+        UILabel *indirizzo = (UILabel *)[cell viewWithTag:1];
+		UILabel *zona = (UILabel *)[cell viewWithTag:2];
+		indirizzo.text = [NSString stringWithFormat:@"%@, %@",
+                          [self.dict objectForKey:@"Indirizzo_Esercente"],
+                          [self.dict objectForKey:@"Citta_Esercente"]];	
+        indirizzo.text = [indirizzo.text capitalizedString];
+        if ([[self.dict objectForKey:@"Zona_Esercente"] isEqualToString:@"<null>"]) {
+            zona.text = @"";
+		}
+        else {
+            zona.text = [NSString stringWithFormat:@"Zona: %@",
+                         [self.dict objectForKey:@"Zona_Esercente"]];
+        }
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    else if ([key isEqualToString:@"GiornoChiusura"]) {
+        [[NSBundle mainBundle] loadNibNamed:@"CellaDettaglio1" owner:self options:NULL] ;
+        cell = CellaDettaglio1;
+        UILabel *giorno = (UILabel *)[cell viewWithTag:1];
+        UILabel *etich = (UILabel *)[cell viewWithTag:2];
+        etich.text = @"Chiusura settimanale";
+        giorno.text = [self.dict objectForKey:@"Giorno_chiusura_Esercente"];
+        giorno.text = [giorno.text capitalizedString];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+
+    }
+    
+    else if ([key isEqualToString:@"GiornoValidità"]) {
+        [[NSBundle mainBundle] loadNibNamed:@"CellaValidita2" owner:self options:NULL];
+        cell = self.cellavalidita;
+        UILabel *etich = (UILabel *)[cell viewWithTag:1];
+        UILabel *validita = (UILabel *)[cell viewWithTag:2];
+        etich.text=@"Giorni di validita della Carta PerDue";
+        
+        NSArray *righe = nil;
+        NSDictionary *diz;
+        int idcontr=[[self.dict objectForKey:@"IDcontratto_Contresercente"]intValue];
+        NSURL *link = [NSURL URLWithString:[NSString stringWithFormat: @"http://www.cartaperdue.it/partner/Validita.php?idcontratto=%d",idcontr]];
+        //NSLog(@"Url: %@", link);
+        
+#warning roba da passare a DBAccess
+        /*
+        NSString *jsonret = [[[NSString alloc] initWithContentsOfURL:link] autorelease];
+        // NSLog(@"%@",jsonret); // Look at the console and you can see what the restults are
+        
+        NSData *jsonRet = [jsonret dataUsingEncoding:NSUTF8StringEncoding];
+        NSError *error = nil;
+        
+        diz = [[CJSONDeserializer deserializer] deserializeAsDictionary:jsonRet error:&error];
+        
+        if (diz) {
+            righe = [diz objectForKey:@"Giorni"];
+        }
+        
+        if ([righe count]==0){ //condizioni assenti
+            validita.text=@"Non disponibile";
+            
+        }
+        else { //costruisco la strinaga condizioni
+            
+            //NSLog(@"La tessera vale per %d giorni settimanali", [righe count]);
+            NSString *giorni=[NSString stringWithFormat:@""];
+            for (int i=0;i<[righe count];i++) {
+                diz = [righe objectAtIndex: i];
+                giorni=[NSString stringWithFormat:@"%@%@ ", giorni,[diz objectForKey:@"giorno_della_settimana"]];
+                
+            }
+            validita.text=[NSString stringWithFormat:@"%@", giorni];				
+            
+            if ( [ [NSString stringWithFormat:@"%@",[self.dict objectForKey:@"Note_Varie_CE"]] isEqualToString:@"<null>"] ){ //non ci sono condizioni
+                cell.selectionStyle = UITableViewCellSelectionStyleNone;
+            }
+            else {
+                cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            }
+        }*/
+
+    }
+    
+    else if ([key isEqualToString:@"Telefono"]) {
+        [[NSBundle mainBundle] loadNibNamed:@"provacella" owner:self options:NULL] ;
+        cell = provacella;
+        UILabel *telefono = (UILabel *)[cell viewWithTag:1];
+        UILabel *etic = (UILabel *)[cell viewWithTag:2];
+        etic.text = @"";
+        telefono.text = [self.dict objectForKey:@"Telefono_Esercente"];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    else if ([key isEqualToString:@"Email"]) {
+        [[NSBundle mainBundle] loadNibNamed:@"provacella" owner:self options:NULL] ;
+        cell = provacella;
+        UILabel *email = (UILabel *)[cell viewWithTag:1];
+        UILabel *etic = (UILabel *)[cell viewWithTag:2];
+        etic.text = @"";
+        email.text = [self.dict objectForKey:@"Email_esercente"];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    else if ([key isEqualToString:@"URL"]) {
+        [[NSBundle mainBundle] loadNibNamed:@"provacella" owner:self options:NULL] ;
+        cell = provacella;
+        UILabel *sitoweb = (UILabel *)[cell viewWithTag:1];
+        UILabel *etic = (UILabel *)[cell viewWithTag:2];
+        etic.text=@"";
+        sitoweb.text = [self.dict objectForKey:@"Url_Esercente"];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    /*
+    if( (cell == nil) && (indexPath.section==0) && (indexPath.row==0) ) {
 		//cell = [[[NSBundle mainBundle] loadNibNamed:@"cellaindirizzo" owner:self options:NULL] objectAtIndex:0];
         [[NSBundle mainBundle] loadNibNamed:@"cellaindirizzo" owner:self options:NULL];
         cell = cellaindirizzo;
@@ -211,9 +330,11 @@
 			
 			}
 		}   
-	}		   	 
+	}	*/	   	 
 	
-	// Configure the cell.
+	
+    /*
+    // Configure the cell.
 	if ( (indexPath.row == 0)&&(indexPath.section==0) ){
 		UILabel *indirizzo = (UILabel *)[cell viewWithTag:1];
 		UILabel *zona = (UILabel *)[cell viewWithTag:2];
@@ -395,15 +516,15 @@
 			cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
 		}
 		
-	}
+	}*/
 
-	   return cell;
+    return cell;
 }
 
 
 #pragma mark - UITableViewDelegate
 
-
+#warning indagare che roba è questa...
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
     UIView *customView = [[[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.bounds.size.width, 44.0)] autorelease];
     [customView setBackgroundColor:[UIColor clearColor]];
@@ -469,76 +590,84 @@
 
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-	if ( (indexPath.row == 0)&&(indexPath.section==0) ){ //apri mappa
-		[NSThread detachNewThreadSelector:@selector(spinTheSpinner) toTarget:self withObject:nil];
+	NSString *key = [self.idxMap keyForIndexPath:indexPath];
+    
+    if ([key isEqualToString:@"Indirizzo"]) { 
 		self.mappa.navigationItem.titleView = self.tipoMappa;
-		
+        //TODO: capire se ste righe relative a map.delegate e map.showUserLocation devono
+        //andare qui...
+        self.map.delegate = self;
+		self.map.showsUserLocation = YES;
 		[self.navigationController pushViewController:self.mappa animated:YES];
 		
-		self.map.delegate = self;
-		self.map.showsUserLocation = YES;
-		float lati, longi;
-		int d;
-		lati=[[self.dict objectForKey:@"Latitudine"] doubleValue];
-		longi=[[self.dict objectForKey:@"Longitudine"] floatValue];
-		d=[[self.dict objectForKey:@"IDesercente"]intValue];
+		double latitude = [[self.dict objectForKey:@"Latitudine"] doubleValue];
+		double longitude =[[self.dict objectForKey:@"Longitudine"] doubleValue];
+		NSInteger idEsercente = [[self.dict objectForKey:@"IDesercente"] intValue];
 		NSString *nome = [self.dict objectForKey:@"Insegna_Esercente"];
-		//NSString *address=[NSString alloc];		
-		//address=[self.dict objectForKey:@"Indirizzo_Esercente"];
-		NSString *address=[[[NSString alloc ]initWithFormat:@"%@, %@",[[self.dict objectForKey:@"Indirizzo_Esercente"]capitalizedString],[[self.dict objectForKey:@"Citta_Esercente"]capitalizedString]] autorelease];
-
-		int ident=d;
-		
-		[self.map addAnnotation:[[[GoogleHQAnnotation alloc] init:lati:longi:nome:address:ident] autorelease]];
+		NSString *address = [NSString stringWithFormat:@"%@, %@",
+                             [[self.dict objectForKey:@"Indirizzo_Esercente"]capitalizedString],
+                             [[self.dict objectForKey:@"Citta_Esercente"]capitalizedString]];
+        GoogleHQAnnotation *ann = [[[GoogleHQAnnotation alloc] init:latitude :longitude :nome :address :idEsercente] autorelease];
+		[self.map addAnnotation:ann];
+#warning levare il deselect
 		[tableView deselectRowAtIndexPath:indexPath animated:YES];
-
-
-
-
 	}	
 	
-	if ( ((indexPath.row == 2)&&(indexPath.section==0)) || ((indexPath.row == 1)&&(indexPath.section==0)&& ([tableView numberOfRowsInSection:0]==2)) ) { //condizioni
-		self.condizioni.title = [NSString stringWithFormat:@"%@",[self.dict objectForKey:@"Insegna_Esercente"]];
-		[self.navigationController pushViewController:self.condizioni animated:YES];
+	if ([key isEqualToString:@"GiornoValidita"] ) {
+		self.condizioni.title = [self.dict objectForKey:@"Insegna_Esercente"];
 		self.cond.text=[NSString stringWithFormat:@"%@",[self.dict objectForKey:@"Note_Varie_CE"]];
+        [self.navigationController pushViewController:self.condizioni animated:YES];
+#warning levare il deselect
 		[tableView deselectRowAtIndexPath:indexPath animated:YES];
 	}
 	
-	
-	if ( (indexPath.row == 0)&&(indexPath.section==1) ){ //telefona o mail o sito
-		if(!( [ [NSString stringWithFormat:@"%@",[self.dict objectForKey:@"Telefono_Esercente"]] isEqualToString:@"<null>"]) ){ //la cella esprime un num di tel
-			PerDueCItyCardAppDelegate *appDelegate = (PerDueCItyCardAppDelegate*)[[UIApplication sharedApplication]delegate];
-			UIActionSheet *aSheet = [[UIActionSheet alloc] initWithTitle:[NSString stringWithFormat:@"Vuoi chiamare\n%@?",[self.dict objectForKey:@"Insegna_Esercente"]] delegate:self cancelButtonTitle:@"Annulla" destructiveButtonTitle:nil otherButtonTitles:@"Chiama", nil];
+	if ([key isEqualToString:@"Telefono"]) { //telefona o mail o sito
+        PerDueCItyCardAppDelegate *appDelegate = (PerDueCItyCardAppDelegate*)[[UIApplication sharedApplication] delegate];
+        NSString *actionSheetTxt = [NSString stringWithFormat:@"Vuoi chiamare\n%@?",
+                                    [self.dict objectForKey:@"Insegna_Esercente"]];
+        UIActionSheet *aSheet = [[UIActionSheet alloc] initWithTitle:actionSheetTxt
+                                                            delegate:self 
+                                                   cancelButtonTitle:@"Annulla"
+                                              destructiveButtonTitle:nil 
+                                                   otherButtonTitles:@"Chiama", nil];
+        [aSheet showInView:appDelegate.window];
+        [aSheet release];			
+#warning levare il deselect
+        [tableView deselectRowAtIndexPath:indexPath animated:YES];		
+    }
+		
+    
+    
+    if ([key isEqualToString:@"Email"]) {
+        MFMailComposeViewController *controller = [[MFMailComposeViewController alloc] init];
+        [[controller navigationBar] setTintColor:[UIColor colorWithRed:142/255.0 
+                                                                 green:21/255.0 
+                                                                  blue:7/255.0
+                                                                  alpha:1.0]];
+        NSArray *to = [NSArray arrayWithObject:[self.dict objectForKey:@"Email_Esercente"]];
+        [controller setToRecipients:to];
+        controller.mailComposeDelegate = self;
+        [controller setMessageBody:@"" isHTML:NO];
+        [self presentModalViewController:controller animated:YES];
+        [controller release];
+#warning levare il deselect
+        [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    }
+    
+    if ([key isEqualToString:@"URL"]) {
+        NSString *urlString = [NSString stringWithFormat:@"http://%@",
+                              [self.dict objectForKey:@"Url_Esercente"]];
+        NSURL *url = [NSURL URLWithString:urlString];
+        NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+        [self.webView loadRequest:requestObj];		
+        self.sito.title = [NSString stringWithFormat:@"%@",[self.dict objectForKey:@"Insegna_Esercente"]];
+        [self.navigationController pushViewController:self.sito animated:YES];
+#warning levare il deselect
+        [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    }
+        
 
-			[aSheet showInView:appDelegate.window];
-			[aSheet release];			
-			
-			[tableView deselectRowAtIndexPath:indexPath animated:YES];		
-		}
-		else {
-			if(!( [ [NSString stringWithFormat:@"%@",[self.dict objectForKey:@"Email_Esercente"]] isEqualToString:@"<null>"]) ){ //la cella esprime un indirizzo email
-				MFMailComposeViewController *controller = [[MFMailComposeViewController alloc] init];
-				[[controller navigationBar] setTintColor:[UIColor colorWithRed:142/255.0 green:21/255.0 blue:7/255.0 alpha:1.0]];
-				NSArray *to = [NSArray arrayWithObject:[NSString stringWithFormat:@"%@",[self.dict objectForKey:@"Email_Esercente"]]];
-				[controller setToRecipients:to];
-				controller.mailComposeDelegate = self;
-				[controller setMessageBody:@"" isHTML:NO];
-				[self presentModalViewController:controller animated:YES];
-				[controller release];
-				[tableView deselectRowAtIndexPath:indexPath animated:YES];
-			}
-			else { //la cella esprime un url
-				[NSThread detachNewThreadSelector:@selector(spinTheSpinner) toTarget:self withObject:nil];
-				NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@",[self.dict objectForKey:@"Url_Esercente"]]];
-				NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
-				[self.webView loadRequest:requestObj];		
-				[self.navigationController pushViewController:self.sito animated:YES];
-				self.sito.title = [NSString stringWithFormat:@"%@",[self.dict objectForKey:@"Insegna_Esercente"]];
-				[tableView deselectRowAtIndexPath:indexPath animated:YES];
-				[self.webView release];
-				self.webView=nil;
-
-			}
+		/*	}
 			
 		}
 		
@@ -584,7 +713,7 @@
 		self.webView=nil;
 		
 		
-	}
+	}*/
 	
 }
 
@@ -651,9 +780,9 @@
 @end
 
 
-
+#pragma mark -
 /*********************************************************************/
-
+#pragma mark -
 
 @interface IndexPathMapper () {
     NSMutableArray *map;
