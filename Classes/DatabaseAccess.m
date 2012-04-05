2a478e0cafaa6c51a3789a0e1f8c40faf4cc757b
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2012-04-05 11:31:09 +0200

    Database Access: aggiunto metodo connessioni get [NON RIPORTARE QUESTO COMMIT SUL MASTER!!]

--- Classes/DatabaseAccess.m
+++ Classes/DatabaseAccess.m
@@ -37,6 +37,38 @@ NSString* key(NSURLConnection* con)
     return [NSString stringWithFormat:@"%p",con];
 }
 
+
+- (void)getConnectionToURL:(NSString *)urlString {
+    NSURL *url = [[[NSURL alloc] initWithString:urlString] autorelease];
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    
+    // Lancio della connessione
+    NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:YES];
+    
+    
+    // Accodamento della connessione e impostazione del buffer in cui ricevere i dati
+    if(connection){
+        NSLog(@"IS CONNECTION TRUE");
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+        
+        // TODO: Ri-approfondire lo scopo di st'oggetto, che lo ricordo solo vagamente
+        [readConnections addObject:connection];
+        
+        NSMutableData *receivedData = [[NSMutableData data] retain];
+        //[connectionDictionary setObject:connection forKey:key(connection)];
+        [dataDictionary setObject:receivedData forKey:key(connection)];
+        //NSLog(@"RECEIVED DATA FROM DICTIONARY : %p",[dataDictionary objectForKey:connection]);
+    }
+    else{
+        NSLog(@"theConnection is NULL");
+        //mostrare alert all'utente che la connessione è fallita??
+    }
+    
+}
+
+
+
 -(void)registerUserOnServer:(NSArray*)userData{
     
     [userData retain];
@@ -752,7 +784,7 @@ NSString* key(NSURLConnection* con)
 
     //NSLog(@"DONE. Received Bytes: %d", [receivedData length]);
     NSString *json = [[NSString alloc] initWithBytes: [receivedData mutableBytes] length:[receivedData length] encoding:NSUTF8StringEncoding];
-    //NSLog(@"JSON  %@", json);
+    NSLog(@"JSON  %@", json);
     
     
     
