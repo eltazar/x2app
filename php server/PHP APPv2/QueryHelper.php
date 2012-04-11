0f9655e68df25350e8370a31f81923efc5999431
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2012-04-11 11:45:08 +0200

    QueryHelper: settata codifica utf8

--- php server/PHP APPv2/QueryHelper.php
+++ php server/PHP APPv2/QueryHelper.php
@@ -12,6 +12,7 @@
             $this->sql_connection = mysql_connect(self::$host, self::$usr, self::$pwd);
             if ($this->sql_connection){
                 mysql_select_db(self::$appDb, $this->sql_connection);
+                $this->raw_query("SET NAMES 'utf8'");
             }
         }
         
@@ -23,13 +24,13 @@
                 while($resultsArray[] = mysql_fetch_object($results)) {  }
                 return json_encode($resultsArray);
             }
-//            $results = mysql_query($query, $this->sql_connection);
-//            if ($results) {
-//                while($resultsArray[] = mysql_fetch_object($results)) {  }
-//                return json_encode($resultsArray);
-//            } else {
-//                return "('Error')";
-//            }
+            //$results = mysql_query($query, $this->sql_connection);
+            //if ($results) {
+            //    while($resultsArray[] = mysql_fetch_object($results)) {  }
+            //    return json_encode($resultsArray);
+            //} else {
+            //    return "('Error')";
+            //}
         }
         
         public function raw_query($query) {
@@ -43,54 +44,12 @@
         public function lastError() {
             return mysql_error($this->sql_connection);
         }
-
+        
         public function __destruct() {
             mysql_close($this->sql_connection);
         }
-                               
-                               
+        
+        
         
     }
-
-
-/* Database credentials
-
-$db2 = 'cartaperdue';
-
-$citta=$_GET['prov'];
-$citta=str_replace("!"," ",$citta); //nel client lo spazio è stato sostituito da un carattere speciale. Qui ricostruingo la stringa
-
-$link = mysql_connect($host, $uid, $pwd) or die("Could not connect");
-
-//select database
-mysql_select_db($db) or die("Could not select database");
-mysql_select_db($db2) or die("Could not select database");
-
-mysql_query("SET NAMES 'utf8' ");
-// Create an array to hold our results
-$arr = array();
-
-$sql="SELECT coupon_tofferte.idofferta,offerta_titolo_breve,idesercente, offerta_foto_big,coupon_valore_acquisto, coupon_valore_facciale
-FROM cartaperdue_coupon.coupon_tofferte,cartaperdue_coupon.tofferte_province ,cartaperdue.wrp_province
-WHERE
-tofferte_province.idofferta=coupon_tofferte.idofferta
-AND wrp_province.provincia='$citta'
-AND tofferte_province.idprovincia=wrp_province.idprovincia 
-AND DATEDIFF(offerta_periodo_al,NOW())>='0'
-AND DATEDIFF(NOW(),offerta_periodo_dal)>='0'
-
-AND offerta_attiva='1'
-ORDER BY posizione
-;";
-
-
-$rs = mysql_query($sql) or die(mysql_error());
-
-// Add the rows to the array 
-while($obj = mysql_fetch_object($rs)) {
-    $arr[] = $obj;
-}
-
-//return the json result. The string users is just a name for the container object. Can be set anything.
-echo '{"Esercente":'.json_encode($arr).'}';*/
 ?>
