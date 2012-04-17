ae033d4c68405be53c2a64d841562a9fbe193f42
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2012-04-17 03:00:03 +0200

    Card.php: controllo esistenza carta in tacquisti_carte_online

--- php server/PHP APPv2/Card.php
+++ php server/PHP APPv2/Card.php
@@ -14,8 +14,15 @@
     
         
     $json_result = $qh->query($query);
+    $results = json_decode($json_result, TRUE);
     
+    if (count($results) >= 2) {
+        echo '{"Card":'.$json_result.'}';
+    } else {
+        $query = "SELECT tcustomer.nome_contatto AS name, tcustomer.cognome_contatto AS surname, tacquisti_carte_online.codice_carta AS number, DATE_FORMAT(tacquisti_carte_online.data_scadenza, '%d/%m/%Y') AS expiryString FROM cartaperdue.tcustomer, cartaperdue_app.tacquisti_carte_online WHERE tcustomer.idcustomer = tacquisti_carte_online.id_utente AND REPLACE(nome_contatto, ' ', '') = REPLACE('$name', ' ','') AND REPLACE(cognome_contatto, ' ', '') = REPLACE('$surname', ' ', '') AND REPLACE(codice_carta, ' ', '') = REPLACE('$number', ' ', '')";
+        $json_result = $qh->query($query);
+        echo '{"Card":'.$json_result.'}';
+    }
     
-    echo '{"Card":'.$json_result.'}';
     
 ?>
