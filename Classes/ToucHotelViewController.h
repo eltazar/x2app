0fa903c2eb5df8c6703ab45b9a5b36c6509ece1c
Mario Greco <mrgreco3@gmail.com>
2012-04-27 16:40:11 +0200

    To_uc_hOt_elViewController: inserita classe per la sua gestione

---
+++ Classes/ToucHotelViewController.h
@@ -0,0 +1,15 @@
+//
+//  ToucHotelViewController.h
+//  PerDueCItyCard
+//
+//  Created by mario greco on 27/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToucHotelViewController : UIViewController
+
+
+-(IBAction)downloadFromAppStore:(id)sender;
+@end
